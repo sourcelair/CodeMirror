dd2e3607bc1aaa4e21c463cf4596cbab501300d9
Paris <paris@sourcelair.com>
2015-12-27 15:16:19 +0200

    Implement `copyState`

--- mode/jsx/jsx.js
+++ mode/jsx/jsx.js
@@ -23,10 +23,9 @@
           name: "xml",
           htmlMode: true,
           multilineTagIndentFactor: parserConfig.multilineTagIndentFactor,
-          multilineTagIndentPastTag: parserConfig.multilineTagIndentPastTag
+          multilineTagIndentPastTag: parserConfig.multilineTagIndentPastTag,
+          allowMissing: true
         }),
-        jsState = jsMode.startState(),
-        xmlState = xmlMode.startState(),
         openingTag = /^\s*<[^/]+>/,
         closingTag = /^\s*<\/.+>/,
         selfClosingTag = /^\s*<.+\/\s*>/;
@@ -38,11 +37,16 @@
         var switchToJS = state.shouldSwitchToJS && state.tagClosed;
 
         if (switchToJS) {
-          state.currentMode = "js";
           // Reset switches
           state.shouldSwitchToJS = false;
           state.tagClosed = false;
+          state.switchToJS = true;
         }
+      }
+
+      if (state.switchToJS) {
+        state.currentMode = "js";
+        state.switchToJS = false;
       }
 
       // If current mode is not XML and an XML opening, closing or self-closing
@@ -52,13 +56,18 @@
                           stream.match(closingTag, false) ||
                           stream.match(selfClosingTag, false);
         if (switchToXML) {
-          state.currentMode = "xml";
+          state.switchToXML = "xml";
         }
+      }
+
+      if (state.switchToXML) {
+        state.currentMode = "xml";
+        state.switchToXML = false;
       }
 
       // If the editor is runing in XML mode and an XML closing or self-closing
       // tag gets detected, we should start with JavaScript-first parsing, after
-      // advancing the next ">" character.
+      // advancing the next ">" character or next "/>".
       if (state.currentMode == "xml" && !state.shouldSwitchToJS) {
         state.shouldSwitchToJS = stream.match(closingTag, false) ||
                                  stream.match(selfClosingTag, false);
@@ -67,23 +76,64 @@
       // If an XML closing or self-closing tag has been detected, make sure to
       // save it in the state, when the tag gets closed.
       if (state.currentMode == "xml") {
-        if (state.shouldSwitchToJS && (stream.peek() == ">")) {
-          state.tagClosed = true;
+        if (state.shouldSwitchToJS) {
+          state.tagClosed = (stream.peek() == ">") ||
+                            (stream.match("/>", false));
         }
       }
 
-      if (state.currentMode == "xml") {
-        return xmlMode.token(stream, xmlState);
+      // Detecting a "{" in XML mode means that a JavaScript expression
+      // is about to follow. We should switch to back to JavaScript mode and
+      // increment the JavaScript expression level by one.
+      // Source: http://facebook.github.io/react/docs/jsx-in-depth.html#javascript-expressions
+      if (state.currentMode == "xml" && stream.peek() == "{") {
+        state.jsExpressionLevel++;
+        state.switchToJS = true;
+        stream.next();
+        return null;
       }
 
-      return jsMode.token(stream, jsState);
+      // Detecting a "}" when in a JavaScript expression means that the
+      // JavaScript expression finished and we should return back to XML mode.
+      if (state.currentMode == "js" && stream.peek() == "}") {
+        state.jsExpressionLevel--;
+
+        // If the JavaScript expression that just finished is an attribute value
+        // the XML parser is still waiting for a valid XML attribute value;
+        // text enclosed in quotes or double-quotes, or a single word. We should
+        // change the state of the XML parser to move forward and not wait for
+        // an XML attribute value.
+        if (state.xmlState.state.name == "attrValueState") {
+          state.xmlState.state = state.xmlState.state("string");
+        }
+
+        state.switchToXML = true;
+        stream.next();
+        return null;
+      }
+
+      if (state.currentMode == "xml") {
+        return xmlMode.token(stream, state.xmlState);
+      }
+
+      return jsMode.token(stream, state.jsState);
     }
 
     return {
       startState: function () {
         return {
           tokenize: tokenBase,
-          currentMode: 'js' // We are defaulting to JavaScript as the initial mode
+          currentMode: 'js', // Default to JavaScript as the initial mode,
+          jsExpressionLevel: 0,
+          jsState: jsMode.startState(),
+          xmlState: xmlMode.startState()
+        };
+      },
+      copyState: function (state) {
+        return {
+          tokenize: state.tokenize,
+          jsState: CodeMirror.copyState(jsMode, state.jsState),
+          xmlState: CodeMirror.copyState(xmlMode, state.xmlState)
         };
       },
       token: function (stream, state) {
